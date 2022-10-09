dad0b1af12294fa12ef0163d9a1d2ba03e302239
bojun1995 <bojun1995@foxmail.com>
2022-10-10 00:13:29 +0800

    [chg]baidu map

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# vite-react-use-map
+1. 安装node环境
+2. 安装node_modules
+
+   ```
+   npm i --registry=https://registry.npmmirror.com
+   ```
+3. 运行
+
+   ```
+   npm run dev
+   ```
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import csvFile from './assets/car.csv?raw'
+import csvFile from './assets/46路-0056.csv?raw'
 
 class MapComp extends React.Component {
   render() {
@@ -9,7 +9,8 @@ class MapComp extends React.Component {
     const map = initMap({
       tilt: 60,
       heading: 0,
-      center: [106.521831, 29.571811],
+      // TODO 地图中心位置
+      center: [118.73933, 31.99809],
       zoom: 14,
       displayOptions: {
         poi: false,
@@ -34,8 +35,40 @@ class MapComp extends React.Component {
       scale: 100,
     })
 
+    // data template
+    // const template = [
+    //   {
+    //     geometry: {
+    //       type: 'LineString',
+    //       coordinates: [
+    //         [116.308528, 40.050574],
+    //         [116.307378, 40.052314],
+    //         [116.306175, 40.054274],
+    //         [116.30515, 40.056608],
+    //         [116.304809, 40.058955],
+    //         [116.304055, 40.060611],
+    //       ],
+    //     },
+    //   },
+    // ]
+
+    let data = []
     const dataSet = mapv.csv.getDataSet(csvFile)
-    const data = dataSet.get().slice(0, 80)
+    const car = {
+      geometry: {
+        type: 'LineString',
+        coordinates: [],
+      },
+    }
+    dataSet._data.some((row, idx) => {
+      if (idx < 100)  {
+        // TODO 字段名
+        car.geometry.coordinates.push([Number(row.pm2d5), Number(row.lon)])
+      } else {
+        return true
+      }
+    })
+    data = [car]
 
     view.addLayer(carlineLayer)
     carlineLayer.setData(data)
