b977be9b73602edbe4c312674b1ed0a6e1ad4fbd
bojun1995 <bojun1995@foxmail.com>
2022-10-09 23:42:32 +0800

    [chg]baidu mpa

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
+import csvObj from './car.csv'
+
+class MapComp extends React.Component {
+  render() {
+    return <div id="map_container"></div>
+  }
+  componentDidMount() {
+    const map = initMap({
+      tilt: 60,
+      heading: 0,
+      center: [106.521831, 29.571811],
+      zoom: 14,
+      displayOptions: {
+        poi: false,
+      },
+      style: purpleStyle,
+      skyColors: [
+        // 地面颜色
+        'rgba(226, 237, 248, 0)',
+        // 天空颜色
+        'rgba(186, 211, 252, 1)',
+      ],
+    })
+
+    const view = new mapvgl.View({
+      map: map,
+    })
+
+    const data = [
+      {
+        geometry: {
+          type: 'LineString',
+          coordinates: [],
+        },
+      },
+      {
+        geometry: {
+          type: 'LineString',
+          coordinates: [],
+        },
+      },
+    ]
+
+    const carlineLayer = new mapvgl.CarLineLayer({
+      url: '/car.gltf',
+      autoPlay: true,
+      step: 0.2,
+      scale: 100,
+    })
+
+    setTimeout(() => {
+      view.addLayer(carlineLayer)
+      carlineLayer.setData(data)
+    }, 500)
+
+    map.setDefaultCursor('default')
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <MapComp />
 }
 
 export default App
